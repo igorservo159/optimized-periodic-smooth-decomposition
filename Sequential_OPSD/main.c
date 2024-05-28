ae9efb7813ddf321baae26718c85e9ed825655b9
igorservo159 <igorservo159@gmail.com>
2024-05-28 19:26:45 -0300

    Implementação do Paper na forma sequencial.

---
+++ Sequential_OPSD/main.c
@@ -0,0 +1,35 @@
+#include "aux.h"
+
+#define RAND_MAX_F 2147483647.0f
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Use: %s <M> <N>\n", argv[0]);
+        return -1;
+    }
+
+    size_t rows = atoi(argv[1]);
+    size_t columns = atoi(argv[2]);
+
+    MKL_Complex8 *I_t = NULL;
+    init(&I_t, rows, columns);
+    fill(I_t, rows, columns, 1);
+
+    show_matrix(I_t, rows, columns);
+
+    MKL_Complex8 *B_t = NULL;
+    init(&B_t, rows, columns);
+
+    compute_periodic_border_B(I_t, B_t, rows, columns);
+    compute_fft2D_column_row(I_t, rows, columns);
+    compute_fft2D_of_B(B_t, rows, columns);
+    compute_smooth_component_S(B_t, rows, columns);
+    compute_periodic_component_P(I_t, B_t, rows, columns);
+
+    show_matrix(I_t, rows, columns);
+
+    free_matrix(B_t);
+    free_matrix(I_t);
+}
---
+++ Tests/Step_D.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <complex.h>
+#include <time.h>
+#include <math.h>
+#include <mkl.h>
+#include <omp.h>
+
+#define RAND_MAX_F 2147483647.0f
+#define PI 3.14159265358979323846
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Use: %s <M> <N>\n", argv[0]);
+        return -1;
+    }
+
+    size_t M = atoi(argv[1]);
+    size_t N = atoi(argv[2]);
+
+    MKL_Complex8 *data = (MKL_Complex8 *)malloc(M * N * sizeof(MKL_Complex8));
+    if (data == NULL)
+    {
+        printf("Erro ao alocar memória\n");
+        return 1;
+    }
+
+    printf("%.2f GB\n", M * N * sizeof(MKL_Complex8) / (1024.0f * 1024.0f * 1024.0f));
+
+    // srand(time(NULL));
+
+    srand(1);
+
+    // printf("\nValores da Imagem:\n");
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            float random_real = (float)rand() / RAND_MAX_F;
+            float scaled_real = random_real * 4 + 1;
+
+            data[i + j * M].real = scaled_real;
+            data[i + j * M].imag = 0.0f;
+
+            // printf("(%.2f, %.2f) ", data[i + j * M].real, data[i + j * M].imag);
+        }
+        // printf("\n");
+    }
+    printf("\n");
+
+    MKL_Complex8 aux = {data[0].real, data[0].imag};
+    // MKL_Complex8 *D = (MKL_Complex8 *)malloc(M * N * sizeof(MKL_Complex8));
+
+    double start = omp_get_wtime();
+
+    /*
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            D[i + j * M].real = (2.0f * cosf(2.0f * PI * i / M) + 2.0f * cosf(2.0f * PI * j / N) - 4.0f);
+            // printf("%.2f + i%.2f ", D[i + j * M].real, D[i + j * M].imag);
+        }
+        // printf("\n");
+    }
+
+    vcDiv(M * N, data, D, data);
+     */
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            data[i + j * M].real /= (2.0f * cosf(2.0f * PI * i / M) + 2.0f * cosf(2.0f * PI * j / N) - 4.0f);
+            data[i + j * M].imag /= (2.0f * cosf(2.0f * PI * i / M) + 2.0f * cosf(2.0f * PI * j / N) - 4.0f);
+        }
+    }
+
+    data[0].real = aux.real;
+    data[0].imag = aux.imag;
+
+    double end = omp_get_wtime();
+    double time_spent = (end - start);
+    printf("%f s\n", time_spent);
+
+    /*
+    printf("\nValores de D:\n");
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("(%.2f, %.2f) ", data[i + j * M].real, data[i + j * M].imag);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    */
+
+    free(data);
+
+    return 0;
+}
