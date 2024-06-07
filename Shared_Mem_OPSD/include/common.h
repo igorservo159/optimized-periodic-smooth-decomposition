bdaba1e24c3df7bf9bb2468bd63facc920753c04
igorservo159 <igorservo159@gmail.com>
2024-06-07 12:22:10 -0300

    Iniciando organização do Shared_Mem_OPSD e testando melhorias de desempenho

---
+++ Shared_Mem_OPSD/include/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define RAND_MAX_F 2147483647.0f
+#define PI 3.14159265358979323846
+
+#include <mkl.h>
+#include "mkl_dfti.h"
+#include <math.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "complex.h"
+#include "time.h"
+#include "mkl.h"
+#include "omp.h"
+
+#endif
